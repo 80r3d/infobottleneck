1464a8505ca0f029dc446759db04f0ffa88f4636
Jay Patel <jay.j.patel@sjsu.edu>
2023-07-11 01:32:17 -0700

    Update infobottleneck.cpp

--- infobottleneck/infobottleneck.cpp
+++ infobottleneck/infobottleneck.cpp
@@ -1,4 +1,4 @@
-// cartpolebalencing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// InformationBottleneck.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
 #include <cmath>
